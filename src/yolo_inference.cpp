5b841aac8a7afa828abbe482d585a397ed4a9a9b
DaniilProgMath <alekseevdaniil73@gmail.com>
2023-02-06 21:34:14 +0500

    fixed nms and added docs

--- src/yolo_inference.cpp
+++ src/yolo_inference.cpp
@@ -5,6 +5,13 @@
 
 std::vector<std::string> Yolov4_tiny::load_class_names(std::string config_path)
 {
+	/*
+		ru:
+        	Функция для загрузки списка классов.
+    	eng:
+        	Function to load the list of classes.
+    */
+
 	std::vector<std::string> class_vector;
 	std::fstream class_file;
 	class_file.open(config_path, std::ios::in);
@@ -25,6 +32,14 @@ void Yolov4_tiny::collect_stream_to_vect(
 	tvm::runtime::NDArray stream, std::vector<std::vector<float>>& vectors,
 	int stream_size, int step)
 {
+	/*
+		ru:
+			Функция выполняющая сбор данных из выходного тензора сети в
+			контейнер с векторами из числа step значений. 
+		eng:
+			A function that collects data from the output tensor of the network 
+			into a container with vectors of step values.
+	*/
 
 	auto stream_ptr = static_cast<float*>(stream->data);
 
@@ -40,6 +55,14 @@ void Yolov4_tiny::collect_stream_to_vect(
 
 void Yolov4_tiny::Mat_to_CHW(float *data, cv::Mat &frame)
 {
+	/*
+		ru:
+			Функция выполняющая преобразование cv::Mat в линейный массив 
+			из нормализованных значений пикселей.
+		eng:
+			A function that converts cv::Mat into a linear array 
+			of normalized pixel values.
+	*/
     assert(data && !frame.empty());
     unsigned int volChl = 416 * 416;
 
@@ -54,28 +77,43 @@ void Yolov4_tiny::Mat_to_CHW(float *data, cv::Mat &frame)
 
 void Yolov4_tiny::load_and_preprocessing_data(cv::Mat image)
 {
-	this->image = image.clone();
-   	cv::cvtColor(image, this->frame, cv::COLOR_BGR2RGB);
-    cv::resize(this->frame, this->input,  cv::Size(416,416));
+	/*
+		ru:
+        	Функция выполняющая препроцессинг изображения и загрузку его во
+        	входной тензор модели.
+
+    	eng:
+        	A function that preprocesses an image and loads it into the model's input tensor.
+	*/
+	cv::Mat resized_image, RGB_image;
+   	cv::cvtColor(image, RGB_image, cv::COLOR_BGR2RGB);
+    cv::resize(RGB_image, resized_image,  cv::Size(416,416));
     float data[416 * 416 * 3];
-    Mat_to_CHW(data, this->input);
-    memcpy(this->x->data, &data, 3 * 416 * 416 * sizeof(float));
-    this->set_input("input", this->x);
+    Mat_to_CHW(data, resized_image);
+    memcpy(this->input->data, &data, 3 * 416 * 416 * sizeof(float));
+    this->set_input("input", this->input);
 }
 
 
 void Yolov4_tiny::run_inference()
 {
+	/*
+		ru:
+			Функция выполняющая инференс модели.
+		eng:
+			The function that performs the inference of the model.
+			
+	*/
 	this->inference_bboxes.clear();
     this->inference_confs.clear();
 
 	this->run();
 	
-	this->get_output(0, y0);
-    this->get_output(1, y1);
+	this->get_output(0, output_0);
+    this->get_output(1, output_1);
 
-    collect_stream_to_vect(y0, this->inference_bboxes, 2535*4, 4);
-    collect_stream_to_vect(y1, this->inference_confs, 2535*80, 80);
+    collect_stream_to_vect(output_0, this->inference_bboxes, 2535*4, 4);
+    collect_stream_to_vect(output_1, this->inference_confs, 2535*80, 80);
 
     this->output_postprocess();
 
@@ -84,8 +122,15 @@ void Yolov4_tiny::run_inference()
 
 void Yolov4_tiny::output_postprocess()
 {
+	/*
+		ru:
+			Функция для обработки результатов выхода сети.
+		eng:
+			A function to process the results of a network output.
+	*/
 	std::vector<float> max_confs;
     std::vector<int> max_inds;
+    std::vector<std::vector<float>> max_bboxes;
 
     this->result_bboxes.clear();
     this->result_scores.clear();
@@ -96,13 +141,20 @@ void Yolov4_tiny::output_postprocess()
     	std::vector<float>::iterator max_conf;
     	max_conf = std::max_element(this->inference_confs.at(i).begin(), 
     								this->inference_confs.at(i).end());
-		max_inds.push_back(std::distance(this->inference_confs.at(i).begin(), max_conf));
-		max_confs.push_back(*max_conf);
+
+    	if(*max_conf > CONFIDENCE_THRESHOLD)
+    	{
+			max_inds.push_back(std::distance(	this->inference_confs.at(i).begin(), 
+												max_conf));
+			max_confs.push_back(*max_conf);
+			max_bboxes.push_back(this->inference_bboxes.at(i));
+		}
     }
+
 
     for(size_t cls = 0; cls < NUM_CLASSES; cls++)
     {
-    	std::vector<cv::Rect> cls_bboxes;
+    	std::vector<cv::Rect2d> cls_bboxes;
     	std::vector<float> cls_scores;
     	std::vector<int> indices;
 
@@ -110,29 +162,32 @@ void Yolov4_tiny::output_postprocess()
     	{
  			if(max_inds.at(i) == cls)
  			{
- 				float x1 = this->inference_bboxes.at(i).at(0) * this->image.cols;
-    			float y1 = this->inference_bboxes.at(i).at(1) * this->image.rows;
-    			float x2 = this->inference_bboxes.at(i).at(2) * this->image.cols;
-    			float y2 = this->inference_bboxes.at(i).at(3) * this->image.rows;
+ 				float x1 = max_bboxes.at(i).at(0);
+    			float y1 = max_bboxes.at(i).at(1);
+    			float x2 = max_bboxes.at(i).at(2);
+    			float y2 = max_bboxes.at(i).at(3);
 
-    			cls_bboxes.push_back(cv::Rect(x1, y1, x2 - x1, y2 - y1));
+    			cls_bboxes.push_back(cv::Rect2d(x1, y1, x2 - x1, y2 - y1));
     			cls_scores.push_back(max_confs.at(i));
  			}
     	}
 
     	cv::dnn::NMSBoxes(cls_bboxes, cls_scores, CONFIDENCE_THRESHOLD, 
-    					  NMS_THRESHOLD, indices, 1.f, 10);
+    					  NMS_THRESHOLD, indices);
 
-    	std::vector<cv::Rect> nmsBoxes;
+    	std::vector<cv::Rect2d> nmsBoxes;
         std::vector<float> nmsConfidences;
         std::vector<int> nmsClassIds;
 
     	for(size_t i = 0; i < indices.size(); i++)
     	{
-    		size_t idx = indices.at(i);
-    		nmsBoxes.push_back(cls_bboxes.at(idx));
-    		nmsConfidences.push_back(cls_scores.at(idx));
-    		nmsClassIds.push_back(cls);
+    		if(i < TOP_DETECTION_COUNT)
+    		{
+	    		size_t idx = indices.at(i);
+	    		nmsBoxes.push_back(cls_bboxes.at(idx));
+	    		nmsConfidences.push_back(cls_scores.at(idx));
+	    		nmsClassIds.push_back(cls);
+	    	}
     	}
 
     	this->result_bboxes.push_back(nmsBoxes);
@@ -142,9 +197,15 @@ void Yolov4_tiny::output_postprocess()
 }
 
 
-json Yolov4_tiny::dump_output_to_json(float timestamp)
+json Yolov4_tiny::dump_output_to_json(int img_width, int img_height, float timestamp)
 {
+	/*
+		ru:
+	        Функция для сохранения результатов детекций в объект json.
 
+	    eng:
+	        A function to collect discovery results in a json object.
+    */
 	json frame_info;
 	json json_detected_objects = json::array();
 
@@ -157,11 +218,16 @@ json Yolov4_tiny::dump_output_to_json(float timestamp)
 			{
 				json json_object;
 
+				int x1 = (int) (this->result_bboxes.at(i).at(j).x * img_width);
+				int y1 = (int) (this->result_bboxes.at(i).at(j).y * img_height);
+				int box_width = (int) (this->result_bboxes.at(i).at(j).width * img_width);
+				int box_height = (int) (this->result_bboxes.at(i).at(j).height * img_height);
+
 				json_object["class"] = class_name;
-				json_object["coords"]["x1"] = this->result_bboxes.at(i).at(j).x;
-				json_object["coords"]["y1"] = this->result_bboxes.at(i).at(j).y;
-				json_object["coords"]["x2"] = this->result_bboxes.at(i).at(j).x + this->result_bboxes.at(i).at(j).width;
-				json_object["coords"]["y2"] = this->result_bboxes.at(i).at(j).y + this->result_bboxes.at(i).at(j).height;
+				json_object["coords"]["x1"] = x1;
+				json_object["coords"]["y1"] = y1;
+				json_object["coords"]["x2"] = x1 + box_width;
+				json_object["coords"]["y2"] = y1 + box_height;
 				json_object["score"] = this->result_scores.at(i).at(j);
 
 				json_detected_objects.push_back(json_object);
